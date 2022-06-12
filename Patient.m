641bd835d8b99a127f48c59f8cbd9303935ff2af
pelinkoc <70231405+pelinkoc@users.noreply.github.com>
2022-06-12 19:58:35 +0300

    Add files via upload

---
+++ Patient.m
@@ -0,0 +1,44 @@
+
+classdef Patient
+    properties
+    name
+    surname
+    priorty
+    day
+    end
+
+    methods
+        function     pri=getPatientPriorty(self)
+
+
+
+        end
+
+      function     setPatientPriorty(self)
+
+
+
+        end
+
+
+        function     day=getPatientDay(self)
+
+
+
+        end
+
+        function     setPatientDay(self)
+
+
+
+        end
+    end
+
+end
+
+
+
+
+
+
+

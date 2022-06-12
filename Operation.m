a9c8fb4e033d199a3affc2f6485767318228340b
ilkayy2 <107369303+ilkayy2@users.noreply.github.com>
2022-06-13 00:17:20 +0300

    Update Operation.m

--- Operation.m
+++ Operation.m
@@ -11,22 +11,32 @@ classdef Operation < handle
     end
 
     methods
-        function operation = Operation(id, patient, availableInterval, scheduledInterval, duration, operationDay)
+        function oprtn = Operation(i, pt, aint, sint, drt, oday)
             %constructor function
+            oprtn.id= i;
+            oprtn.patient= pt;
+            oprtn.availableInterval= aint;
+            oprtn.scheduledInterval= sint;
+            oprtn.duration= drt;
+            oprtn.operationDay= oday;
+
         end
 
         
-        function setScheduleledInterval(self)
+        function setScheduleledInterval(self, sint)
             %A setter method to set scheduledInterval property of the object Operation when it is scheduled.
+            self.scheduledInterval= sint;
         end
 
 
 
-        function setAvailableInterval(self)
+        function setAvailableInterval(self, aint)
             %A setter method to set availableInterval property of the object Operation when the operation is to be postponed to the next day.
+            self.availableInterval= aint;
         end  
     end
 end    
+
 
 
 

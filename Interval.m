d71accda45b45b991311ebceb6a06086906524e8
pelinkoc <70231405+pelinkoc@users.noreply.github.com>
2022-06-12 23:50:33 +0300

    Update Interval.m

--- Interval.m
+++ Interval.m
@@ -1,3 +1,4 @@
+% Pelin Koc 2444800 İlkay Koc 2444792
 classdef Interval < handle
 % An Interval has a left endpoint and a right endpoint.
     
@@ -16,31 +17,40 @@ classdef Interval < handle
         function w = getWidth(self)
         % Return the width of the Interval
             %%%% %MISSION Write your code below %%%%
+            w=self.right-self.left;
             
         end
         
         function scale(self, f)
         % Scale self by a factor f
             %%%% %MISSION Write your code below %%%%
+            w= self.right - self.left;
+            self.right= self.left + w*f;
+
                     
         end
         
         function shift(self, s)
         % Shift self by constant s
             %%%% %MISSION Write your code below %%%%
+            self.right=self.right+s;
+            self.left=self.left+s;
             
         end
         
         function tf = isIn(self, other)
         % tf is true (1) if self is in the other Interval
             %%%% %MISSION Write your code below %%%%
-        
+         tf= self.left>=other.left && self.right<=other.right;
+
+         
         end
         
         function Inter = add(self, other)
         % Inter is the new Interval formed by adding self and the 
         % the other Interval
             %%%% %MISSION Write your code below %%%%
+            Inter=self+other;
             
         end
         
